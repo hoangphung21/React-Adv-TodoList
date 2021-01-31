8b24d1935ff9fc65d738e1c49173b8c4c50712da
hoangphung21 <phunghoang211195@gmail.com>
2021-01-30 21:20:03 -0500

    settings for firebase

--- react-adv-todolist/src/firebase.js
+++ react-adv-todolist/src/firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyBIqDwDz-Wd89K8LZ_GaAgEh7-vg1KTr7c",
+    authDomain: "todolist-adv-react.firebaseapp.com",
+    databaseURL: "https://todolist-adv-react-default-rtdb.firebaseio.com",
+    projectId: "todolist-adv-react",
+    storageBucket: "todolist-adv-react.appspot.com",
+    messagingSenderId: "136405839754",
+    appId: "1:136405839754:web:758204c339715a4be0a273",
+
 });
 
 export { firebaseConfig as firebase };

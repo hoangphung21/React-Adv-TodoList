ddec8f6e59d911a1383acd0c9303812229dd8678
hoangphung21 <phunghoang211195@gmail.com>
2021-02-02 15:09:04 -0500

    adding react hooks

--- react-adv-todolist/src/hooks/index.js
+++ react-adv-todolist/src/hooks/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 import { firebase } from '../firebase';
-
-const collatedTasksExist = () => { };
+import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
@@ -19,5 +19,21 @@ export const useTasks = selectedProject => {
                 : selectedProject === 'INBOX' || selectedProject === 0
                     ? (unsubcribe = unsubcribe.where('date', '==', ''))
                     : unsubcribe;
-    }, []);
+
+        unsubcribe = unsubcribe.onSnapshot(snapShot => {
+            const newTasks = snapShot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
+
+            setTasks(
+                selectedProject === 'NEXT_7'
+                    ? newTasks.filter(
+                        task =>
+                            moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 && task.archived !== true)
+
+                    : newTasks.filter(task => task.archived !== true)
+            );
+        });
+    }, [selectedProject]);
 }
